b333a49ac669154d8e2d82ba87e1478f55d528dd
Gajesh Bhat <gajeshbht@gmail.com>
2018-07-03 23:27:53 +0530

    Added Earthquakes list  visualization.

--- UCSDUnfoldingMaps/src/guiexperiments/EarthquakeMarker.java
+++ UCSDUnfoldingMaps/src/guiexperiments/EarthquakeMarker.java
@@ -2,14 +2,18 @@ package guiexperiments;
 
 
 import java.awt.Color;
+import java.util.List;
 
 import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.providers.OpenStreetMap;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
-
+import parsing.ParseFeed;
+import java.util.ArrayList;
 public class EarthquakeMarker extends PApplet {
 
 	/**
@@ -17,6 +21,7 @@ public class EarthquakeMarker extends PApplet {
 	 */
 	private static final long serialVersionUID = 1L;
 	private UnfoldingMap earthquakeMap;
+	private String earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
 	
 	public SimplePointMarker getquakeLocationMarker(Location quakeLocation) {
 		SimplePointMarker quakeLocationMarker = new SimplePointMarker(quakeLocation);
@@ -36,18 +41,26 @@ public class EarthquakeMarker extends PApplet {
 	
 	// Gets the final marker i.e driver for other getters and setters of marker and its properties.
 	
-	public SimplePointMarker getquakeLocationMapMarker(double latitude,double longitude) {
-		Location earthquakeLocation = this.getquakeLocation(latitude, longitude);
+	public SimplePointMarker getquakeLocationMapMarker(Location earthquakeLocation) {
 		SimplePointMarker earthquakeLocationMarker = this.getquakeLocationMarker(earthquakeLocation);
 		setMarkerColorYellow(earthquakeLocationMarker);
 		return earthquakeLocationMarker;
 	}
 	
+	// Returns the markers list
+	
+	public List<Marker> getearthquakeLocationMarkerMapList() {
+		List<Marker> markers = new ArrayList<Marker>();
+		List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
+		for (PointFeature pointFeature : earthquakes) {
+			markers.add(getquakeLocationMapMarker(pointFeature.location));
+		}
+		return markers;
+	}
 	
 	public void setup() {
-	earthquakeMap = new UnfoldingMap(this, 200, 50, 700, 500,new OpenStreetMap.OpenStreetMapProvider());	
-	SimplePointMarker chillequakeMarker = getquakeLocationMapMarker(-38.14f, -73.04f); 
-	earthquakeMap.addMarker(chillequakeMarker);
+	earthquakeMap = new UnfoldingMap(this, 200, 50, 700, 500,new OpenStreetMap.OpenStreetMapProvider());	 
+	earthquakeMap.addMarkers(getearthquakeLocationMarkerMapList());
 	MapUtils.createDefaultEventDispatcher(this, earthquakeMap);
 	earthquakeMap.zoomToLevel(2);
 	}
